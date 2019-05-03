338dfe72301c977beac14fa24a6a958543860a5a
Radoslav Sharapanov <radoslav.sharapanov@paysafe.com>
2019-05-03 16:12:27 +0300

    ...

--- js/script.babel.js
+++ js/script.babel.js
@@ -1,1 +1,1 @@
-console.log("Accordion init");console.log("Tooltip init");(function(){console.log("Hello World");console.log("a")})();
+"use strict";console.log("Accordion init");console.log("Tooltip init");(function(){console.log("Hello World");console.log("a")})();
